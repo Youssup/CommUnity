d98d1da0b3de3ccd9e3a8d8e5e42ebabd886ef71
Yoosup Song <Youssupsong@gmail.com>
2024-11-09 12:50:59 -0500

    Impemented Express Application

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require("express")
+const path = require("path")
+const app = express()
+app.use(express.json());
+
+app.use(express.static('client/dist'))
+
+//CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+});
+
+const communityRoutes = require("./routes/communityRoutes")
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+
+const PORT = 3000 || process.env.PORT
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
