5cc9a110293867e953dd99815b33263d251140fa
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-11-09 17:46:56 -0500

    finsihing event forms (not pretty yet)

--- client/src/components/MakeEvent.vue
+++ client/src/components/MakeEvent.vue
@@ -18,18 +18,34 @@ const isOpen = ref(false)
         <label class="input input-bordered flex items-center gap-2">
   <input type="text" class="grow" placeholder="Title" />
 </label>
-    <textarea class="textarea textarea-bordered" placeholder="Description"></textarea>
+    <textarea class="textarea textarea-bordered flex items-centered gap-2" placeholder="Description"></textarea>
 
+    <label class="input input-bordered flex flex-row-reverse items-center gap-2 cursor-pointer">
+  <input type="date" class="grow bg-transparent focus:outline-none" placeholder="MM/DD/YYYY" />
+</label>
 
 <label for="timeInput" class="input input-bordered flex items-center gap-2 cursor-pointer">
   <input id="timeInput" type="time" class="grow" value="time" />
 </label>
+
+<h4>Address</h4>
+<label class="input input-bordered flex items-center gap-2">
+  <input type="text" class="grow" placeholder="Street" />
+</label>
+<label class="input input-bordered flex items-center gap-2">
+  <input type="text" class="grow" placeholder="City" />
+</label>
+<label class="input input-bordered flex items-center gap-2">
+  <input type="text" class="grow" placeholder="State" />
+</label>
+<label class="input input-bordered flex items-center gap-2">
+  <input type="string" class="no-spinner grow" placeholder="Zip Code" />
+</label>
+
     </section>
     <div class="modal-action">
       <form method="dialog">
-        <!-- if there is a button in form, it will close the modal -->
-
-        <button class="btn">Close</button>
+        <button class="btn bg-green-500 text-white">Submit</button>
       </form>
     </div>
   </div>
@@ -50,5 +66,10 @@ button{
 .textarea{
     margin: 0.5rem;
     min-width: 430px;
+}
+.no-spinner::-webkit-outer-spin-button,
+.no-spinner::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
