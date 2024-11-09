84035ffc506ab8c4a90a194a7984007c4a7f7139
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-11-09 11:41:44 -0500

    changing view folder to pages and adding navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <header>
+    <NavBar />
+  </header>
   <RouterView />
 </template>
 
