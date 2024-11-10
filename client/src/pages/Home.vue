636f1f81bf18f7ca305e715251aaa98629efab38
Yoosup Song <Youssupsong@gmail.com>
2024-11-09 19:38:27 -0500

    Created Home page

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -3,50 +3,41 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div
-  class="hero min-h-screen"
-  style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);">
-  <div class="hero-overlay bg-opacity-60"></div>
-  <div class="hero-content text-neutral-content text-center">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Welcome</h1>
-      <p class="mb-5">
-        Our App is used to help communities by showing them what type of organizations and clubs are around them. We can see community events,
-        clubs, meetings, and organiztions infos with rating from members and the people to help you find exactly what your looking for.
-      </p>
-
-      <p class="mb-5">
-        Now lets see some clubs around your community!
-      </p>
-      <RouterLink to="/clubPro">
-      <button class="btn btn-primary">See Clubs</button>
-    </RouterLink>
+<div class="screen-h-min h-screen flex flex-col">
+    <div class="dark:bg-transparent">
+        <div class="mx-auto flex flex-col items-center py-12 sm:py-24">
+            <div class="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col mb-5 sm:mb-10">
+                <h1
+                    class="text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl text-center text-gray-800 dark:text-black font-black leading-10">
+                    Find your
+                    <span class="text-violet-800 dark:text-violet-500">community</span>
+                </h1>
+                <p class="mt-5 sm:mt-10 lg:w-10/12 text-gray-600 dark:text-gray-900 font-normal text-center text-xl">
+                    Find the best community for you to join and share your thoughts and ideas with others.
+                </p>
+            </div>
+            <div class="flex w-11/12 md:w-8/12 xl:w-6/12">
+                <div class="flex rounded-md w-full">
+                    <input type="text" name="q"
+                        class="w-full p-3 rounded-md rounded-r-none border border-2 border-gray-300 placeholder-current dark:bg-gray-500  dark:text-gray-300 dark:border-none "
+                        placeholder="location" />
+                    <button
+                        class="inline-flex items-center gap-2 bg-violet-700 text-white text-lg font-semibold py-3 px-6 rounded-r-md">
+                        <span>search</span>
+                        <svg class="text-gray-200 h-5 w-5 p-0 fill-current" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px"
+                            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;"
+                            xml:space="preserve">
+                            <path
+                                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                        </svg>
+                </button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 </template>
 
 <style scoped>
-.input {
-  min-width: 300px;
-  background-color: #f5f5f5;
-  color: #242424;
-  padding: .15rem .5rem;
-  min-height: 60px;
-  border-radius: 4px;
-  outline: none;
-  border: none;
-  line-height: 1.15;
-  box-shadow: 0px 10px 20px -18px;
-}
-
-input:focus {
-  border-bottom: 2px solid #5b5fc7;
-  border-radius: 4px 4px 2px 2px;
-}
-
-input:hover {
-  outline: 1px solid lightgrey;
-}
-
 </style>
