b39055eaa115aa5ac5aeaa401159a551f2407374
Yoosup Song <Youssupsong@gmail.com>
2024-11-10 10:40:02 -0500

    Fixed Spelling error

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const communityController = require('../controllers/reviewController.js');
+const reviewController = require('../controllers/reviewController.js');
 
 // Route to create a new review
 router.post('/', reviewController.createReview);
