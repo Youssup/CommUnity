3b8878147106946ffca6f408f8c3f934c2be6bb2
Yoosup Song <Youssupsong@gmail.com>
2024-11-09 21:56:55 -0500

    Switching to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use((req, res) => {
 });
 
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
 PASSWORD = process.env.PASSWORD
